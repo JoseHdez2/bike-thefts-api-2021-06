b54cae5620dc1c400c83f59208f76ad75654c75d
José Manuel Hernández <josemanuelhdez2@gmail.com>
2021-06-24 14:42:38 +0100

    add autogenerated swagger docs.

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('Bike Theft API')
+    .setDescription('API for reporting bike thefts.')
+    .setVersion('0.1')
+    .build();
+  const swaggerDoc = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('/', app, swaggerDoc);
+
   await app.listen(3000);
 }
 bootstrap();

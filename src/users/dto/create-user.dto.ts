2866c3ae0a6443ff730adc8020376e4725797534
José Manuel Hernández <josemanuelhdez2@gmail.com>
2021-06-25 13:58:32 +0100

    validation

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,11 +1,17 @@
 import { ApiProperty } from "@nestjs/swagger";
+import { IsAlpha, IsEmail, MaxLength } from "class-validator";
 import { User } from "../entities/user.entity";
 
 export class CreateUserDto {
     @ApiProperty({example: 'John'})
+    @IsAlpha()
+    @MaxLength(100)
     name: string;
     @ApiProperty({required: false}) 
+    @IsAlpha()
+    @MaxLength(200)
     surnames?: string;
     @ApiProperty({example: 'john_doe@email.com'}) 
+    @IsEmail()
     email: string;
 }
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,5 +1,5 @@
 import { Body, Controller, Get, NotFoundException, Param, ParseIntPipe, Post, Query } from '@nestjs/common';
-import { ApiNotFoundResponse, ApiOkResponse, ApiQuery, ApiTags } from '@nestjs/swagger';
+import { ApiBadRequestResponse, ApiCreatedResponse, ApiNotFoundResponse, ApiOkResponse, ApiQuery, ApiTags } from '@nestjs/swagger';
 import { CreateUserDto } from './dto/create-user.dto';
 import { User } from './entities/user.entity';
 import { UsersService } from './users.service';
@@ -29,6 +29,8 @@ export class UsersController {
     return user;
   }
 
+  @ApiCreatedResponse({type: User})
+  @ApiBadRequestResponse()
   @Post()
   createUser(@Body() body: CreateUserDto): User {
     return this.usersService.create(body);

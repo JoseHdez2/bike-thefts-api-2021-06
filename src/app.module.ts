1a469d522795eb01286384d7d1fd025d07d4c447
José Manuel Hernández <josemanuelhdez2@gmail.com>
2021-06-25 14:21:03 +0100

    set up persistence

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,13 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { BikeTheftsModule } from './bike-thefts/bike-thefts.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import config from '../ormconfig';
 
 @Module({
-  imports: [UsersModule, BikeTheftsModule],
+  imports: [TypeOrmModule.forRoot({}), 
+    UsersModule,  
+    BikeTheftsModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,6 +1,13 @@
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
 export class User {
+    @PrimaryGeneratedColumn()
     id: number;
+    @Column()
     name: string;
+    @Column()
     surnames?: string;
+    @Column()
     email: string;
 }

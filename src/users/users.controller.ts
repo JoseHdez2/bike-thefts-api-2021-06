6916c08cd0ecf220bce66694350f8dfe7be790bb
José Manuel Hernández <josemanuelhdez2@gmail.com>
2021-06-25 12:20:18 +0100

    api ok response in swagger doc

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,5 +1,5 @@
 import { Body, Controller, Get, Param, Post } from '@nestjs/common';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiOkResponse, ApiTags } from '@nestjs/swagger';
 import { CreateUserDto } from './dto/create-user.dto';
 import { User } from './entities/user.entity';
 import { UsersService } from './users.service';
@@ -9,11 +9,13 @@ import { UsersService } from './users.service';
 export class UsersController {
   constructor(private usersService: UsersService) {}
 
+  @ApiOkResponse()
   @Get()
   getUsers(): User[] {
     return this.usersService.findAll();
   }
 
+  @ApiOkResponse({type: User})
   @Get(':id')
   getUserById(@Param('id') id: string): User {
     return this.usersService.findById(Number(id));

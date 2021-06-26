cb2681d1c77d058c591dfaac12518d5af70b23e0
Jose Hernandez <josemanuelhdez2@gmail.com>
2021-06-26 18:30:44 +0100

    status endpoint

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,14 @@
 import { Controller, Get } from '@nestjs/common';
+import { ApiQuery, ApiResponse } from '@nestjs/swagger';
 import { AppService } from './app.service';
 
 @Controller('api')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get('hello')
-  getHello(): string {
-    return this.appService.getHello();
+  @ApiResponse({description: 'Returns server status.'})
+  @Get('status')
+  getHello(): number {
+    return this.appService.getStatus();
   }
 }

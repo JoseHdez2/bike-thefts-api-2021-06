25d6982e0b8506bb49d7dee66a9e94a4d2066a73
Jose Hernandez <josemanuelhdez2@gmail.com>
2021-06-27 15:11:05 +0100

    create bike example

--- src/bike-thefts/dto/create-bike-theft.dto.ts
+++ src/bike-thefts/dto/create-bike-theft.dto.ts
@@ -4,22 +4,30 @@ import { IsAlpha, IsAlphanumeric, MaxLength } from "class-validator";
 export class CreateBikeTheftDto {
     
     id: number;
+
+    @ApiProperty({example: 'ABCD1234', required: false})
     @MaxLength(50)
     bikeLicenseNumber?: string;
-    @IsAlpha()
+    
+    @ApiProperty({example: 'red', required: false})
     @MaxLength(50)
     bikeColor?: string;
-    @IsAlphanumeric()
+    
+    @ApiProperty({example: 'mountain bike', required: false})
     bikeType?: string;
-    @ApiProperty({example: 'John Smith'})
-    @IsAlpha()
+    
+    @ApiProperty({example: 'John Smith', required: false})
     @MaxLength(300)
     ownerFullName?: string;
-    @ApiProperty({example: '2021-06-28'})
+    
+    @ApiProperty({example: '2021-06-28', required: false})
     theftDate?: Date;
+    
+    @ApiProperty({example: 'I stopped for a coffee for 5 minutes and when I came back my bike was gone.', required: false})
     @MaxLength(1000)
     theftDescription?: string;
-    @IsAlphanumeric()
+    
+    @ApiProperty({example: 'I stopped for a coffee for 5 minutes and when I came back my bike was gone.', required: false})
     @MaxLength(500)
     theftAddress?: string;
 }

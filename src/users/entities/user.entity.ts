528351f4df0ff0709ff6eaa1b5d640961cea0e75
Jose Hernandez <josemanuelhdez2@gmail.com>
2021-06-28 02:09:24 +0100

    user and policeDepartment relation.

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,13 +1,20 @@
-import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+import { PoliceDepartment } from "src/police-departments/entities/police-department.entity";
+import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from "typeorm";
 
 @Entity()
 export class User {
     @PrimaryGeneratedColumn()
     id: number;
+    
     @Column()
     name: string;
+    
     @Column()
     surnames?: string;
+
     @Column({unique: true})
     email: string;
+
+    @ManyToOne(type => PoliceDepartment, pd => pd.id)
+    policeDepartment?: PoliceDepartment;
 }

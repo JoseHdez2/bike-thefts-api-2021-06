f65601176b74998e82fb371368acfaece9b7ad5a
José Manuel Hernández <josemanuelhdez2@gmail.com>
2021-06-28 15:10:41 +0100

    bike thefts crud.

--- src/bike-thefts/bike-thefts.service.ts
+++ src/bike-thefts/bike-thefts.service.ts
@@ -1,32 +1,48 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { BikeTheftEventsService } from 'src/bike-theft-events/bike-theft-events.service';
+import { QueryFailedError, Repository } from 'typeorm';
 import { CreateBikeTheftDto } from './dto/create-bike-theft.dto';
 import { UpdateBikeTheftDto } from './dto/update-bike-theft.dto';
 import { BikeTheft, BikeTheftStatus } from './entities/bike-theft.entity';
 
 @Injectable()
 export class BikeTheftsService {
+
+  constructor(
+    @InjectRepository(BikeTheft) private bikeTheftsRepository: Repository<BikeTheft>,
+    private bikeTheftEventService: BikeTheftEventsService
+  ) {}
+
   create(createBikeTheftDto: CreateBikeTheftDto) {
-    
-    const bikeTheft: BikeTheft = {
-      ...createBikeTheftDto, 
-      caseStatus: BikeTheftStatus.UNASSIGNED
-    };
-    return 'This action adds a new bikeTheft';
+    try {
+      const bikeTheft = this.bikeTheftsRepository.create({ ...createBikeTheftDto, caseStatus: BikeTheftStatus.REPORTED});
+      this.bikeTheftEventService.create({bikeTheftId: bikeTheft.id, caseStatus: bikeTheft.caseStatus});
+      return this.bikeTheftsRepository.save(bikeTheft);
+    } catch (err) {
+        if(err instanceof QueryFailedError){
+            console.log(err);
+        }
+        throw err;
+    }
   }
 
   findAll() {
-    return `This action returns all bikeThefts`;
+    return this.bikeTheftsRepository.find();
   }
 
   findOne(id: number) {
-    return `This action returns a #${id} bikeTheft`;
+    return this.bikeTheftsRepository.findOneOrFail(id);
   }
 
-  update(id: number, updateBikeTheftDto: UpdateBikeTheftDto) {
-    return `This action updates a #${id} bikeTheft`;
+  async update(id: number, updateBikeTheftDto: UpdateBikeTheftDto) {
+    const bikeTheft = await this.findOne(id);
+    const newBikeTheft = { ...bikeTheft, ...updateBikeTheftDto };
+    return this.bikeTheftsRepository.save(newBikeTheft);
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} bikeTheft`;
+  async remove(id: number) {
+    const bikeTheft = await this.findOne(id);
+    return this.bikeTheftsRepository.remove(bikeTheft);
   }
 }

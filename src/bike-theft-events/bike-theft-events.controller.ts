2d35cca5b53ce30875dd00f45dee7fd0a1925687
José Manuel Hernández <josemanuelhdez2@gmail.com>
2021-06-28 14:28:38 +0100

    make events read-only

--- src/bike-theft-events/bike-theft-events.controller.ts
+++ src/bike-theft-events/bike-theft-events.controller.ts
@@ -1,16 +1,9 @@
 import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
 import { BikeTheftEventsService } from './bike-theft-events.service';
-import { CreateBikeTheftEventDto } from './dto/create-bike-theft-event.dto';
-import { UpdateBikeTheftEventDto } from './dto/update-bike-theft-event.dto';
 
 @Controller('bike-theft-events')
 export class BikeTheftEventsController {
   constructor(private readonly bikeTheftEventsService: BikeTheftEventsService) {}
-
-  @Post()
-  create(@Body() createBikeTheftEventDto: CreateBikeTheftEventDto) {
-    return this.bikeTheftEventsService.create(createBikeTheftEventDto);
-  }
 
   @Get()
   findAll() {
@@ -20,15 +13,5 @@ export class BikeTheftEventsController {
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.bikeTheftEventsService.findOne(+id);
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateBikeTheftEventDto: UpdateBikeTheftEventDto) {
-    return this.bikeTheftEventsService.update(+id, updateBikeTheftEventDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.bikeTheftEventsService.remove(+id);
   }
 }
--- src/bike-theft-events/dto/update-bike-theft-event.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/swagger';
-import { CreateBikeTheftEventDto } from './create-bike-theft-event.dto';
-
-export class UpdateBikeTheftEventDto extends PartialType(CreateBikeTheftEventDto) {}
